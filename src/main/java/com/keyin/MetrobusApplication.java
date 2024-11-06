bc23095d16f3d386ef320847dd1d855016d60e46
Danny <shiva.biera@keyin.com>
2024-11-05 23:41:18 -0330

    created main file

---
+++ src/main/java/com/keyin/MetrobusApplication.java
@@ -0,0 +1,11 @@
+package com.keyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrobusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MetrobusApplication.class, args);
+    }
+}
